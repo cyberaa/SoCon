f71964a7c2c30f52522f4d5991ca3f20aaf69a1c
Joao Jesus <jjesusdei@gmail.com>
2012-12-20 21:11:53 +0000

    Revert ""
    This reverts commit d31d55e1c2d22ac4036a5bf5ea76ae8645843367.

--- src/es/cli/MensagemPrivada.java
+++ src/es/cli/MensagemPrivada.java
@@ -14,14 +14,14 @@ public class MensagemPrivada extends Mensagem{
     private String anexos;
     private Utilizador destinatário;
 
-    public MensagemPrivada(String anexos, Utilizador destinatário, String assunto, String texto_mensagem, Date data_criada, Date data_envio, String localizacao_imagem, Utilizador emissor,int delay) {
-        super(assunto, texto_mensagem, data_criada, data_envio, localizacao_imagem, emissor,delay);
+    public MensagemPrivada(String anexos, Utilizador destinatário, String assunto, String texto_mensagem, Date data_criada, Date data_envio, String localizacao_imagem, Utilizador emissor) {
+        super(assunto, texto_mensagem, data_criada, data_envio, localizacao_imagem, emissor);
         this.anexos = anexos;
         this.destinatário = destinatário;
     }
 
-    public MensagemPrivada(String assunto, String texto_mensagem,Utilizador destinatário, Date data_criada, Date data_envio, String localizacao_imagem, Utilizador emissor,int delay) {
-        super(assunto, texto_mensagem, data_criada, data_envio, localizacao_imagem, emissor,delay);
+    public MensagemPrivada(String assunto, String texto_mensagem,Utilizador destinatário, Date data_criada, Date data_envio, String localizacao_imagem, Utilizador emissor) {
+        super(assunto, texto_mensagem, data_criada, data_envio, localizacao_imagem, emissor);
     }
 
     public String getAnexos() {
